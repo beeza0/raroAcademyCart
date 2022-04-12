4b813080c2e01b4b933c941a83f7bfca3816aa77
Bernardo Andrade <bpires99@gmail.com>
2022-04-12 17:46:05 -0300

    final-commit

--- src/components/Cart/index.tsx
+++ src/components/Cart/index.tsx
@@ -1,4 +1,4 @@
-import { Dispatch, SetStateAction, useEffect, useState } from "react";
+import { Dispatch, SetStateAction } from "react";
 import { CloseOutline } from "@styled-icons/evaicons-outline";
 
 import Button from "../Button";
